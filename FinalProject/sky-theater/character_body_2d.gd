54ed82522aa5a71ddbe05af127e08d74a5622016
Mordred321 <zhuang21@sva.edu>
2024-12-09 15:00:11 -0500

    3sceneadded

--- FinalProject/sky-theater/character_body_2d.gd
+++ FinalProject/sky-theater/character_body_2d.gd
@@ -10,15 +10,22 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		
+	var direction := Input.get_axis("ui_left", "ui_right")
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		animated_sprite_2d.play("jump")
 		velocity.y = JUMP_VELOCITY
-	
+		if direction == 1:
+			animated_sprite_2d.flip_h = true
+		else:
+			animated_sprite_2d.flip_h = false
+
+
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		animated_sprite_2d.play("run")
 		velocity.x = direction * SPEED
@@ -29,5 +36,6 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		animated_sprite_2d.play("default")
+		
 
 	move_and_slide()
