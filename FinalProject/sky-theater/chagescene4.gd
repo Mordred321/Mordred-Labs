d51a825f427e15532d25eb6a895ef932c7044a16
Mordred321 <zhuang21@sva.edu>
2024-12-18 07:15:14 -0500

    Story

--- FinalProject/sky-theater/chagescene4.gd
+++ FinalProject/sky-theater/chagescene4.gd
@@ -3,10 +3,11 @@ extends Area2D
 
 var entered = false
 
+func _on_body_entered(body: PhysicsBody2D):
+	entered = true
+	if body.is_in_group("player"):
+		get_tree().change_scene_to_file("res://scene6.tscn")
+
 func _on_body_exited(body: PhysicsBody2D):
 	entered = false
-
-func _process(body):
-	entered = true
-	if Input. is_action_just_pressed("interact"):
-		get_tree().change_scene_to_file("res://scene6.tscn")
+	
