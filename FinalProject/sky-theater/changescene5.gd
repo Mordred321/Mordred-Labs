588991d001117ace4ae3f5b254a4dabb0ac3dc69
Mordred321 <zhuang21@sva.edu>
2025-05-10 20:58:25 -0400

    Revert "Story"
    This reverts commit d51a825f427e15532d25eb6a895ef932c7044a16.

--- FinalProject/sky-theater/changescene5.gd
+++ FinalProject/sky-theater/changescene5.gd
@@ -3,12 +3,10 @@ extends Area2D
 
 var entered = false
 
-func _on_body_entered(body: PhysicsBody2D):
-	entered = true
-	if body.is_in_group("player"):
-		get_tree().change_scene_to_file("res://scene7.tscn")
-
 func _on_body_exited(body: PhysicsBody2D):
 	entered = false
-	
-		
+
+func _process(body):
+	entered = true
+	if Input. is_action_just_pressed("interact"):
+		get_tree().change_scene_to_file("res://scene7.tscn")
